cf3e9f748b3f0b9f222732f77305f0d88e06e671
Tiffany <thlc@uw.edu>
2022-11-13 14:04:27 -0800

    Update #2

--- source/chart_3.R
+++ source/chart_3.R
@@ -11,3 +11,18 @@ df <- df %>%
   mutate(location = paste0(country, ", ", region))
 View(df)
 
+average_score_location_df <-  df %>% 
+  filter(score == mean(score, na.rm = TRUE)) %>% 
+  pull(location)
+print(average_score_location)
+
+average_score_data <- df %>%
+  filter(location == average_score_location) %>% 
+  group_by(location)
+View(average_score_data)
+
+chart_3 <- ggplot(data = average_score_data, mapping = aes(x = year, y = score)) + geom_point() + 
+  ggtitle("Scores Over the Years for the Location with the AVERAGE Transparent Government", 
+          subtitle = "Location:")
+print(chart_3)
+
