90b487cf5fde7b2bd9f41022d600d682f4ae9c70
Tiffany <thlc@uw.edu>
2022-11-14 15:07:53 -0800

    ggplot + para to Chart 1

--- source/chart_1.R
+++ source/chart_1.R
@@ -20,7 +20,20 @@ highest_score_data <- df %>%
   filter(location == highest_score_location)
 View(highest_score_data)
 
-chart_1 <- ggplot(data = highest_score_data, mapping = aes(x = year, y = score)) + geom_point() + 
-          ggtitle("Scores Over the Years for the Location with the Most Transparent Government", 
-          subtitle = "Location: Denmark, WE/EU")
+chart_1 <- ggplot(data = highest_score_data, mapping = aes(x = year, y = score)) + geom_point(shape=1) + 
+          ggtitle("Scores Over the Years for the Location with the
+                  Most Transparent Government", 
+          subtitle = "Location: Denmark, WE/EU") + 
+          theme_minimal() + 
+          xlab("Year") +
+          ylab("Score")
+
+# Description: This scatterplot displays the scores over the years for the location with the most
+# transparent government. This chart has two numerical variables: year and score, and it shows
+# the changes of scores over the years. The score describes the corruption/transparency level, with
+# 100 being the most transparent and 0 being the most corrupt. There is an slight increase from 2012 
+# to 2014, then a decrease from 2014 to 2017, and then it roughly stays the same. Since the score 
+# has not increased nor decreased from 2017, people believe the government is transparent and 
+# their opinion about them has not changed.
+
 print(chart_1)
